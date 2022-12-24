9946ec427b4e9ae568e16ba4e9ec59f3b7ef50c6
volstr <volstr@yandex.ru>
2022-12-24 17:38:17 +0300

    sha1

--- openvpn-install-routeros7.sh
+++ openvpn-install-routeros7.sh
@@ -273,7 +273,7 @@ ca ca.crt
 cert server.crt
 key server.key
 dh dh.pem
-auth SHA512
+auth SHA1
 tls-crypt tc.key
 topology subnet
 server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
@@ -421,7 +421,7 @@ nobind
 persist-key
 persist-tun
 remote-cert-tls server
-auth SHA512
+auth SHA1
 cipher AES-256-CBC
 ignore-unknown-option block-outside-dns
 block-outside-dns
